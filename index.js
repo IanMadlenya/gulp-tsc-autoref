b30a6946934e421db3a2668ea0066aa10729c56e
Philip Kovac <philip.kovac@iweave.com>
2016-07-11 12:04:49 -0400

    Fixed whitespace.

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
-var through         = require('through2');
-var path            = require('path');
-var gutil           = require('gulp-util');
-var header          = require('gulp-header');
+var through			= require('through2');
+var path			= require('path');
+var gutil			= require('gulp-util');
+var header			=require('gulp-header');
 var fs				= require('fs');
-var PluginError     = gutil.PluginError;
+var PluginError		= gutil.PluginError;
 
 const PLUGIN_NAME = 'gulp-tsc-autoref';
 
 function gulpTscAutoref() {
 
-    var searchPattern = /import[\s+].+=[\s+]?(.+);/g;
+	var searchPattern = /import[\s+].+=[\s+]?(.+);/g;
 
-    return through.obj(function(file, enc, callback) {
-        if (file.isStream()) {
-            this.emit('error', new PluginError(PLUGIN_NAME, 'Streams are not supported!'));
-            return callback();
-        }
+	return through.obj(function(file, enc, callback) {
+		if (file.isStream()) {
+			this.emit('error', new PluginError(PLUGIN_NAME, 'Streams are not supported!'));
+			return callback();
+		}
 
-        if (file.isBuffer()) {
-            var match;
-            var references = [];
-            var contents = file.contents.toString();
-            while ((match = searchPattern.exec(contents)) !== null) {
+		if (file.isBuffer()) {
+			var match;
+			var references = [];
+			var contents = file.contents.toString();
+			while ((match = searchPattern.exec(contents)) !== null) {
 				var refNoExt = match[1].replace(/\./g, '/');
 				['.tsx', '.ts'].forEach(function(ext) {
 					var refext = refNoExt + ext;
@@ -31,15 +31,15 @@ function gulpTscAutoref() {
 					} catch (e) {
 					}
 				});
-            }
+			}
 			
-            references.forEach(function(ref) {
+			references.forEach(function(ref) {
 				var fileDir = path.dirname(file.path);
 				var refDir = path.dirname(path.join(file.base, ref));
 				var refFileName = path.basename(ref);					
 				var relativePathToRef = path.relative(fileDir, refDir);
 
-                var relativePath = path.join(relativePathToRef, refFileName);				
+				var relativePath = path.join(relativePathToRef, refFileName);				
 
 				var stream = header('/// <reference path="${path}"/>\n', {path: relativePath});
 
@@ -48,13 +48,13 @@ function gulpTscAutoref() {
 				});
 
 				stream.write(file);
-            });
+			});
 
-        }
+		}
 
-        this.push(file);
-        callback();
-    });
+		this.push(file);
+		callback();
+	});
 };
 
 module.exports = gulpTscAutoref;
